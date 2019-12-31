62145e47a8aac80f556a7887b6345097fefdd7ea
tynr426 <tynr426@126.com>
2019-12-31 14:02:00 +0800

    pulgin

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,6 +3,7 @@ const webpack = require('webpack');
 const merge = require('webpack-merge');
 // // 引入通用webpack配置文件
 const common = require('./webpack.common.js');
+const WasmModuleWebpackPlugin = require('wasm-module-webpack-plugin');
 
 module.exports = merge(common, {
     // 使用 source-map
@@ -31,10 +32,28 @@ module.exports = merge(common, {
                 changeOrigin: true,     // target是域名的话，需要这个参数，
                 secure: false,          // 设置支持https协议的代理
             }
-        }
+        },
+        
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                include: [ path.join(__dirname, './gps') ],
+                use: {
+                  loader: 'babel-loader',
+                  options: {
+                    presets: ['@babel/preset-env'],
+                    plugins: [ '@babel/plugin-syntax-dynamic-import', WasmModuleWebpackPlugin ]
+                  }
+                }
+              }
+        ]
     },
     plugins: [
         new webpack.HotModuleReplacementPlugin(),
+        new WasmModuleWebpackPlugin.WebpackPlugin(),
     ],
     // 设置出口文件地址与文件名
     output: {
